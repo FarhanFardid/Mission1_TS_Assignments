137121b78a0b49d26b86a7226c1e21fe36ac6869
FarhanFardid <farhanfardid009@gmail.com>
2025-05-08 22:21:17 +0600

    problem2 solved

--- assignments.ts
+++ assignments.ts
@@ -1,7 +1,6 @@
 function formatString(input: string, toUpper?: boolean): string{
     if(toUpper === false){
         return input.toLowerCase();
-
     }
     else{
         return input.toUpperCase();
